30a76e48d61faba17fb02962648e7565b6139e8c
Océane Hebrard <oceane47.heb@gmail.com>
2024-02-01 20:46:36 +0100

    Composant qui affiche la liste de toutes les transactions

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const TransactionList = () => {
+
+const { transactions } = useContext(GlobalContext);
+const { deleteTransaction } = useContext(GlobalContext);
+
+//Afficher la croix de suppression d'une transaction au survol 
+const [isShown, setIsShown] = useState(false);
+
   return (
     <div className='transaction-list'>
       <h4 className='history'>Historique</h4>
-      <div>
-        <div className='transaction-item'>
-            <span>Cash</span><span>+500</span>
-        </div>
-      </div>
+      <ul>
+        {transactions.map((transaction) => (
+          <li key={transaction.id} className={transaction.amount > 0 ? 'transaction-item plus' : 'transaction-item minus'} onMouseEnter={() => setIsShown(transaction.id)} onMouseLeave={() => setIsShown(0)}>
+
+            {/* Libellé et montant de la transaction */}
+            {transaction.text}<span>{transaction.amount > 0 ? '+': '-'}{Math.abs(transaction.amount)} €</span>
+
+            {/* Boutton de suppression */}
+            {isShown === transaction.id && <button onClick={() => deleteTransaction(transaction.id)} className='delete-btn'>x</button>}
+
+            </li>
+        ))}
+      </ul>
     </div>
   )
 }
