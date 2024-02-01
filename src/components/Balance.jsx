45dee706d8bd6c7820074a6610ebd8ac78802deb
Océane Hebrard <oceane47.heb@gmail.com>
2024-02-01 20:44:48 +0100

    Composant pour afficher le solde du compte de l'utilisateur

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const Balance = () => {
+  const {transactions} = useContext(GlobalContext);
+  const sum = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+
   return (
     <div className='balance'>
       <h4>Votre solde</h4>
-      <p>260.00 €</p>
+      <p>{sum.toFixed(2)} €</p>
     </div>
   )
 }
