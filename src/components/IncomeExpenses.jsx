d78fbc5698ea9a639127f9e8e001e8cd4e9fc1ae
Océane Hebrard <oceane47.heb@gmail.com>
2024-02-01 20:45:57 +0100

    Composant pour afficher le montant des revenus et des dépenses

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+  
+  //Sum of positive amounts
+  const sumIncome = transactions.reduce((acc, transaction) => {
+    if(transaction.amount > 0 ) {
+      acc += transaction.amount;
+    }
+    return acc;
+  }, 0);
+  //Sum of negative amounts
+  const sumExpense = transactions.reduce((acc, transaction) => {
+    if(transaction.amount < 0) {
+      acc += transaction.amount;
+    }
+    return acc;
+    
+  }, 0)
+  
+    
   return (
     <div className='income-expense'>
       <div className='income'>
         <h4>Income</h4>
-        <p>500.00 €</p>
+        <p>{sumIncome.toFixed(2)} €</p>
       </div>
       <div></div>
       <div className='expense'>
         <h4>Expense</h4>
-        <p>240.00 €</p>
+        <p>{Math.abs(sumExpense).toFixed(2)} €</p>
       </div>
     </div>
   )
