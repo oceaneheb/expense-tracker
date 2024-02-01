047873aeb74f4d840e8627e7288ebe76ae7b087a
Océane Hebrard <oceane47.heb@gmail.com>
2024-02-01 20:47:37 +0100

    Modification du style et du contenu du composant principal App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Balance from './components/Balance'
@@ -6,19 +5,23 @@ import IncomeExpense from './components/IncomeExpenses'
 import TransactionList from './components/TransactionList'
 import AddTransaction from './components/AddTransaction'
 
+import { GlobalProvider } from './context/GlobalState';
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='app'>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpense />
-        <TransactionList />
-        <AddTransaction />
+    <GlobalProvider>
+      <div className='app'>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpense />
+          <TransactionList />
+          <AddTransaction />
+        </div>
       </div>
-    </div>
+    </GlobalProvider>
   )
 }
 
